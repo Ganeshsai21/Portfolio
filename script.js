ce3063b2c2aad24f9ec9f1eae67d97c09701e4f6
Ganesh Dasu <ganeshdasu123@gmail.com>
2025-07-03 10:37:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,34 @@
       menu.classList.toggle('active');
   }
 
+ particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#a27bf8" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#a27bf8",
+      opacity: 0.3,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.8
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" }
+    }
+  },
+  retina_detect: true
+});
 
